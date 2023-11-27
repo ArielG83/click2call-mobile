f2b44e5e558683462b6d5c765375836398ab8f3e
ArielG83 <gromanariel@gmail.com>
2023-11-27 13:15:20 +0200

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView } from "react-native";
+import { WebView } from "react-native-webview";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  return <SafeAreaView style={{ flex: 1 }}>
+      <WebView
+        source={{uri: "https://shop.multi-travel.com/helpcenter-support-request"}}
+        mediaPlaybackRequiresUserAction={false}
+        style={styles.container}
+      />
+    </SafeAreaView>
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#cfcfcf",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
